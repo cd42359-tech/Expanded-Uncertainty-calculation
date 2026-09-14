a7f8d8aad8b1736f3f93a50cddffb70374795f0d
cd42359-tech <cd42359@gmail.com>
2026-09-14 20:07:46 +0530

    Update uncertainty_calculator.py

--- uncertainty_calculator.py
+++ uncertainty_calculator.py
@@ -1,1 +1,48 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title("Calibration Uncertainty Calculator")
+
+st.subheader("Caliper Uncertainty Calculation")
+instrument = st.selectbox(
+    "Select Instrument Type",
+    ["Caliper"]
+)
+option = st.selectbox(
+    "Select Nominal Size",
+    [
+        "150 mm (LC 0.01)",
+        "200 mm (LC 0.01)",
+        "200 mm (LC 0.02)",
+        "300 mm (LC 0.01)",
+        "300 mm (LC 0.02)",
+        "600 mm (LC 0.02)"
+    ]
+)
+if option == "150 mm (LC 0.01)":
+    nominal_size = 150
+    least_count = 0.01
+
+elif option == "200 mm (LC 0.01)":
+    nominal_size = 200
+    least_count = 0.01
+
+elif option == "200 mm (LC 0.02)":
+    nominal_size = 200
+    least_count = 0.02
+
+elif option == "300 mm (LC 0.01)":
+    nominal_size = 300
+    least_count = 0.01
+
+elif option == "300 mm (LC 0.02)":
+    nominal_size = 300
+    least_count = 0.02
+
+else:
+    nominal_size = 600
+    least_count = 0.02
+st.write("Nominal Size :", nominal_size)
+
+st.write("Least Count :", least_count)
