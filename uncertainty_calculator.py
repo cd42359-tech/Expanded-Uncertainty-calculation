7ac9d380082f11b38d7145b60910be9caa6395a1
cd42359-tech <cd42359@gmail.com>
2026-09-14 20:20:16 +0530

    Update uncertainty_calculator.py

--- uncertainty_calculator.py
+++ uncertainty_calculator.py
@@ -79,3 +79,9 @@ readings = [r1, r2, r3, r4, r5]
 
 mean_reading = np.mean(readings)
 st.write("Mean Reading :", round(mean_reading, 6))
+std_dev = np.std(readings, ddof=1)
+
+st.write(
+    "Standard Deviation :",
+    round(std_dev, 6)
+)
