451934dc2235810b306a28e7734c401592865279
cd42359-tech <cd42359@gmail.com>
2026-09-25 14:43:07 +0530

    Update uncertainty_calculator.py

--- uncertainty_calculator.py
+++ uncertainty_calculator.py
@@ -8,14 +8,18 @@ MASTER_BUDGET = {
         "75-100": {}
     },
 
-    "Caliper": {
     "0-150": {
-        "Accuracy of Caliper Checker": {
-            "value": 5.0,
-            "distribution": "Rectangular",
-            "dof": float("inf")
-        }
-    }
+    "Accuracy of Caliper Checker": {"value": 5.0, "distribution": "Rectangular", "dof": float("inf")},
+    "Uncertainty of Caliper Checker": {"value": 3.8, "distribution": "Normal", "dof": float("inf")},
+    "Accuracy of 5mm Master Ring": {"value": 1.1, "distribution": "Rectangular", "dof": float("inf")},
+    "Uncertainty of 5mm Master Ring": {"value": 2.0, "distribution": "Normal", "dof": float("inf")},
+    "Accuracy of 25mm Master Ring": {"value": 0.2, "distribution": "Rectangular", "dof": float("inf")},
+    "Uncertainty of 25mm Master Ring": {"value": 1.0, "distribution": "Normal", "dof": float("inf")},
+    "Accuracy of 10mm Pin": {"value": 0.5, "distribution": "Rectangular", "dof": float("inf")},
+    "Uncertainty of 10mm Pin": {"value": 0.7, "distribution": "Normal", "dof": float("inf")},
+    "Accuracy of Slip Gauge 25mm": {"value": 0.3, "distribution": "Rectangular", "dof": float("inf")},
+    "Uncertainty of Slip Gauge 25mm": {"value": 0.1, "distribution": "Normal", "dof": float("inf")},
+    "Resolution of Vernier": {"value": "dynamic", "distribution": "Rectangular", "dof": float("inf")}
 },
 
     "Plug Gauge": {
