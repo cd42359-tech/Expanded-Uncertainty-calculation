ed3d303754dce08eedd1e2b6dca8c49df88d9df1
cd42359-tech <cd42359@gmail.com>
2026-09-25 13:09:26 +0530

    Update uncertainty_calculator.py

--- uncertainty_calculator.py
+++ uncertainty_calculator.py
@@ -1,30 +1,53 @@
+import streamlit as st
+
 MASTER_BUDGET = {
     "Micrometer": {
-        "0-25": {...},
-        "25-50": {...},
-        "50-75": {...},
-        "75-100": {...}
+        "0-25": {},
+        "25-50": {},
+        "50-75": {},
+        "75-100": {}
     },
 
     "Caliper": {
-        "0-150": {...},
-        "0-200": {...},
-        "0-300": {...},
-        "0-600": {...}
+        "0-150": {},
+        "0-200": {},
+        "0-300": {},
+        "0-600": {}
     },
 
     "Plug Gauge": {
-        "0.5-60": {...},
-        "60-200": {...}
+        "0.5-60": {},
+        "60-200": {}
     },
 
     "Plunger Dial": {
-        "0-1": {...},
-        "0-10": {...}
+        "0-1": {},
+        "0-10": {}
     },
 
     "Dial Gauge": {
-        "0-25": {...}
+        "0-25": {}
     }
 }
-``
+
+
+def get_budget(instrument, selected_range):
+    return MASTER_BUDGET[instrument][selected_range]
+
+
+st.title("NABL Uncertainty Calculator")
+
+instrument = st.selectbox(
+    "Select Instrument",
+    list(MASTER_BUDGET.keys())
+)
+
+selected_range = st.selectbox(
+    "Select Range",
+    list(MASTER_BUDGET[instrument].keys())
+)
+
+budget = get_budget(instrument, selected_range)
+
+st.success(f"Selected Instrument : {instrument}")
+st.success(f"Selected Range : {selected_range}")
